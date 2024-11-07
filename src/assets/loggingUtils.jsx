cc9914047184c3bc14be08aeb91ebba04d91e78d
Ahmed Belabdia <ab.belabdia@gmail.com>
2024-11-07 17:47:37 +0100

    add userId to log events

--- src/assets/loggingUtils.jsx
+++ src/assets/loggingUtils.jsx
@@ -1,16 +1,22 @@
 import axios from 'axios';
+import { useAuth } from '../context/AuthContext';
 
 const logsUrl = "https://flash-language-5bfe9-default-rtdb.europe-west1.firebasedatabase.app/logs.json";
 
 export const logEvent = (eventType, { isCorrect, score, currentWordId }) => {
+    
+    const {currentUser} = useAuth();
+    const userId = currentUser ? currentUser.uid : null ;
     const timestamp = new Date().toISOString();
+    
 
     const logEntry = {
         timestamp,
         eventType,
         isCorrect,
         score,
-        currentWordId // Store only the ID of the current word
+        currentWordId,
+        userId
     };
 
     axios.post(logsUrl, logEntry)
